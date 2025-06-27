e40d492d9ef198e38e81e73f69495c38aa012e5d
Lijiawei60 <1845181626@qq.com>
2025-06-27 11:41:03 +0800

    1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import LoginView from '../Views/LoginView.vue';
 import RegisterView from '../Views/RegisterView.vue'; // 导入注册页面组件
+import UserProfile from '../components/UserProfile.vue';
+import UserInfoEdit from '../components/UserInfoEdit.vue';
+import UserOrderList from '../components/UserOrderList.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,6 +16,30 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Register',
     component: RegisterView, // 添加注册页面路由
   },
+{
+    path: '/user',
+    name: 'UserProfile',
+    component: UserProfile,
+    children: [
+      {
+        path: 'edit',
+        name: 'UserInfoEdit',
+        component: UserInfoEdit,
+      },
+      {
+        path: 'orders',
+        name: 'UserOrderList',
+        component: UserOrderList,
+      },
+      { 
+       path: '', 
+      redirect: to => {
+        return { name: 'UserInfoEdit' }; 
+      }
+    },
+    ],
+  },
+ 
 ];
 
 const router = createRouter({
